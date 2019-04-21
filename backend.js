46c20354d95bb13b578640097311eba54135b65b
erica-cohen <erica.cohen93@gmail.com>
2019-04-21 15:06:01 -0700

    Added most everything except for icons

--- backend.js
+++ backend.js
@@ -8,20 +8,9 @@ var mysql = require('mysql');
 
 const ejs = require('ejs');
 
-//var con = mysql.createConnection({
-//    host: "",
-//    database: "",
-//    user: "",
-//    password: "",
-//    debug: "false"
-//});
+var con;
 
-//con.connect(function(err) {
-//    if (err) throw err;
-//    console.log("connected to db");
-//});
-
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
 app.use(express.json());
 
@@ -31,6 +20,30 @@ app.set('view engine', 'html');
 
 
 console.log('web service started');
+
+function handleDisconnect() {
+  con = mysql.createConnection(process.env.CLEARDB_DATABASE_URL); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  con.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  con.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect(); //create original connection
+
 /**
 * get request
 */
@@ -39,6 +52,155 @@ app.get('/', function (req, res) {
     
     res.render('index', function(err, html) {
       res.send(html);
+    });
+});
+
+app.get('/notes', function (req, res) {
+    con.query("SELECT * from notes;", function(err, result, fields) {
+        if(err) console.log(err);
+        res.send(result);
+    });
+});
+
+app.get('/tasks', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    con.query("SELECT name, completed from tasks where note_id = ? order by idx asc", [req.query.noteId], function(err, result, fields) {
+        if(err) console.log(err);
+        res.send(result);
+    });
+})
+
+function updateNoteTasks(res, noteId, name, tasks) {
+    con.query("DELETE FROM tasks where note_id = ?", [noteId], function(err, result, fields) {
+        if(err) {
+            con.rollback(function() {
+                throw err;
+          });
+        } 
+        else if(tasks.length === 0) {
+            con.commit(function(err) {
+                if (err) { 
+                  con.rollback(function() {
+                    throw err;
+                  });
+                }
+                console.log('Transaction Complete. No tasks.');
+                res.send({
+                    note_id: noteId,
+                    name: name
+                });
+              });
+        }
+        else {
+            let values = [];
+            for(let i = 0; i < tasks.length; i++) {
+                let row = [];
+                row.push(noteId);
+                row.push(tasks[i].idx);
+                row.push(tasks[i].name);
+                row.push(tasks[i].completed);
+                values.push(row);
+            }
+            con.query("INSERT INTO tasks (note_id, idx, name, completed) VALUES ?", [values], function(err, result, fields) {
+                if(err) {
+                    con.rollback(function() {
+                        throw err;
+                  });
+                } else {
+                    con.commit(function(err) {
+                        if (err) { 
+                          con.rollback(function() {
+                            throw err;
+                          });
+                        }
+                        console.log('Transaction Complete.');
+                        res.send({
+                            note_id: noteId,
+                            name: name
+                        });
+                      });
+                }
+            });
+        }
+    });
+    
+}
+
+function saveNewNote(res, name, tasks) {
+    con.query("INSERT INTO notes (name) VALUES (?)", [name], function(err, result, fields) {
+        if(err) {
+            con.rollback(function() {
+                throw err;
+          });
+        } else {
+            let noteId = result.insertId;
+            updateNoteTasks(res, noteId, name, tasks);
+        }
+    });
+}
+              
+function updateNote(res, noteId, name, tasks) {
+    con.query("UPDATE notes set name = ? where note_id = ?", [name, noteId], function(err, result, fields) {
+        if(err) {
+            con.rollback(function() {
+                throw err;
+          });
+        } else {
+            updateNoteTasks(res, noteId, name, tasks);
+        }
+    });   
+}
+
+app.post('/save', function(req, res) {
+    let noteId = req.query.noteId;
+    let name = req.query.name;
+    let body = req.body;
+
+    con.beginTransaction(function(err) {
+        if (err) { throw err; }
+        if(noteId < 0) {
+            saveNewNote(res, name, body);
+        } else {
+            updateNote(res, noteId, name, body);
+        }
+    });
+});
+
+
+function deleteNote(res, noteId) {
+    con.query("DELETE FROM tasks where note_id = ?", [noteId], function(err, result, fields) {
+        if(err) {
+            con.rollback(function() {
+                throw err;
+          });
+        } else {
+            con.query("DELETE FROM notes where note_id = ?", [noteId], function(err, result, fields) {
+                if(err) {
+                    con.rollback(function() {
+                        throw err;
+                  });
+                } else {
+                    con.commit(function(err) {
+                        if (err) { 
+                          con.rollback(function() {
+                            throw err;
+                          });
+                        }
+                        console.log('Delete Transaction Complete.');
+                        res.send({
+                            note_id: noteId
+                        });
+                      });
+                }
+            });
+        }
+    });
+}
+
+app.delete('/', function(req, res) {
+    con.beginTransaction(function(err) {
+        if (err) { throw err; }
+        deleteNote(res, req.query.noteId);
     });
 });
 
@@ -58,5 +220,6 @@ app.get('/sticky-note.png', function(req, res) {
     res.sendFile(__dirname + "/" + "sticky-note.png");
 });
 
-app.listen(3000);
+//app.listen(process.env.PORT);
+app.listen(process.env.PORT);
 
--- tasks.js
+++ tasks.js
@@ -1,10 +1,175 @@
 (function() {
     
     function addNote() {
-        alert("add that note");
+        showPopup(-1, "", []);
+    }
+    
+    function showNote() {
+        let noteId = this.getAttribute('note-id');
+        let name = this.querySelector("h4").innerText;
+        fetch("http://localhost:3000/tasks?noteId=" + noteId, {
+          method: 'GET'
+        }).then(res => res.json())
+        .then(resData => {
+            showPopup(noteId, name, resData);
+        });
+    }
+    
+    function loadNotes() {
+        fetch("http://localhost:3000/notes", {
+          method: 'GET'
+        }).then(res => res.json())
+        .then(resData => {
+            for(let i = 0; i <resData.length; i++) {
+                addNoteToBoard(resData[i]);
+            }
+        });
+    }
+    
+    function addNoteToBoard(noteData) {
+        let notes = document.getElementById("notes");
+        let snote = document.createElement("span");
+        snote.classList = "sticky-note";
+        snote.setAttribute('note-id', noteData['note_id']);
+        snote.innerHTML = "<h4>" + noteData.name + "</h4>";
+
+        snote.onclick = showNote;
+        notes.appendChild(snote);
+    }
+    
+    function insertTask(name, completed) {
+        let task = document.createElement("li");
+        task.innerHTML = "<span>" + name + "</span>";
+        let del = document.createElement("span");
+        del.innerHTML = "del";
+        del.onclick = function() {
+            //grandparent (ul) should remove parent (li)
+            this.parentNode.parentNode.removeChild(this.parentNode);
+        }
+        let check = document.createElement("input");
+        check.setAttribute("type","checkbox");
+        check.onchange = function(){
+            let btask= this.parentNode;
+            btask.parentNode.removeChild(btask);
+            if(this.checked === true){
+                compTasks.appendChild(btask);
+            }
+            else{
+                todoTasks.appendChild(btask);
+            }
+        }
+        task.prepend(check);
+        task.appendChild(del);
+        if(completed === 1) {
+            document.getElementById("completed").appendChild(task);
+            check.checked=true;
+        } else {
+            document.getElementById("todo").appendChild(task);
+        }
+    }
+    
+    function showPopup(noteId, name, tasks) {
+        console.log("SHOW", noteId);
+        let content = document.getElementById("popupcontent");
+        content.setAttribute('note-id', noteId);
+        document.getElementById("name").value = name;
+        
+        //add tasks
+        let todoTasks = document.getElementById("todo");
+        let compTasks = document.getElementById("completed");
+        
+        todoTasks.innerHTML = "";
+        compTasks.innerHTML = "";
+        
+        for(let i = 0; i < tasks.length; i++) {
+            insertTask(tasks[i].name, tasks[i].completed);
+        }
+        
+        //finally show it
+        let popup = document.getElementById("popup");
+        popup.style["display"] = "block";
+    }
+    
+    function addNewTask() {
+        let task = document.getElementById("newtask");
+        let taskName = task.value;
+        task.value = "";
+        insertTask(taskName, 0);
+    }
+    
+    function hidePopup() {
+        let popup = document.getElementById("popup");
+        popup.style["display"] = "none";
+    }
+    
+    function saveNote() {
+        let noteId = parseInt(document.getElementById("popupcontent").getAttribute("note-id"));
+        
+        let body = [];
+        let idx = 0;
+        let todoTasks = document.getElementById("todo");
+        let compTasks = document.getElementById("completed");
+        
+        for(let i = 0; i < todoTasks.childNodes.length; i++) {
+            body.push({
+                idx: idx++,
+                name: todoTasks.childNodes[i].childNodes[1].innerText,
+                completed: 0
+            });
+        }
+        
+        for(let i = 0; i < compTasks.childNodes.length; i++) {
+            body.push({
+                idx: idx++,
+                name: compTasks.childNodes[i].childNodes[1].innerText,
+                completed: 1
+            });
+        }
+        
+        let name = document.getElementById("name").value;
+        fetch("http://localhost:3000/save?noteId=" + noteId + "&name=" + name, {
+          method: 'POST',
+          body: JSON.stringify(body),
+          headers: {
+            "Content-Type": "application/json",
+            // "Content-Type": "application/x-www-form-urlencoded",
+          }
+        }).then(res => res.json())
+        .then(resData => {
+            console.log(resData);
+            if(noteId < 0) {
+                //new note
+                addNoteToBoard(resData);
+            }
+            hidePopup()
+        });
+        console.log("SAVE", noteId, body);
+    }
+    
+    function deleteNote() {
+        let noteId = parseInt(document.getElementById("popupcontent").getAttribute("note-id"));
+        fetch("http://localhost:3000/?noteId=" + noteId, {
+          method: 'DELETE'
+        }).then(res => res.json())
+        .then(resData => {
+            console.log(resData);
+            if(noteId === parseInt(resData['note_id'])) {
+                let toDel = document.querySelector(".sticky-note[note-id='" + noteId + "']");
+                toDel.parentNode.removeChild(toDel);
+            }
+            hidePopup()
+        });
     }
     
     window.onload = function() {
         document.getElementById("add").onclick=addNote;
+        
+        document.getElementById("close").onclick=hidePopup;
+        
+        document.getElementById("save").onclick=saveNote;
+        document.getElementById("delete").onclick=deleteNote;
+        document.getElementById("addtask").onclick=addNewTask;
+        loadNotes();
+        hidePopup();
     }
 })();
