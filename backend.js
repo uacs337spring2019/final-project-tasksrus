446ab40834e4d622e0058e5143efb660a14462b0
erica-cohen <erica.cohen93@gmail.com>
2019-04-21 15:59:51 -0700

    added comments, finished validator checks, rel urls

--- backend.js
+++ backend.js
@@ -1,5 +1,11 @@
 "use strict";
-
+/*
+* author: Erica Cohen
+* date: 04/21/19
+* class: CSC337 Spring 2019
+* backend.js - for final project, handles notes/tasks being added/removed from db (heroku),
+* as well as serving of static files
+*/
 
 const express = require("express");
 const app = express();
@@ -21,6 +27,9 @@ app.set('view engine', 'html');
 
 console.log('web service started');
 
+/*
+* found this function on the internet, to help with heroku cleardb disconnecting us
+*/
 function handleDisconnect() {
   con = mysql.createConnection(process.env.CLEARDB_DATABASE_URL); // Recreate the connection, since
                                                   // the old one cannot be reused.
@@ -45,16 +54,17 @@ function handleDisconnect() {
 handleDisconnect(); //create original connection
 
 /**
-* get request
+* get request, serves static index.html
 */
 app.get('/', function (req, res) {
-	res.header("Access-Control-Allow-Origin", "*");
-    
     res.render('index', function(err, html) {
       res.send(html);
     });
 });
 
+/*
+* get all the notes from the db
+*/
 app.get('/notes', function (req, res) {
     con.query("SELECT * from notes;", function(err, result, fields) {
         if(err) console.log(err);
@@ -62,22 +72,32 @@ app.get('/notes', function (req, res) {
     });
 });
 
+/*
+* get endpoint to get tasks for a note
+*/
 app.get('/tasks', function (req, res) {
-    res.header("Access-Control-Allow-Origin", "*");
-    con.query("SELECT name, completed from tasks where note_id = ? order by idx asc", [req.query.noteId], function(err, result, fields) {
+    //return the tasks for a given noteId
+    con.query("SELECT name, completed from tasks where note_id = ? order by idx asc", 
+              [req.query.noteId], function(err, result, fields) {
         if(err) console.log(err);
         res.send(result);
     });
 })
 
+/*
+* save the tasks and return the note object
+*/
 function updateNoteTasks(res, noteId, name, tasks) {
+    //we will blanket delete any tasks for the note to handle deleted items/reordering
     con.query("DELETE FROM tasks where note_id = ?", [noteId], function(err, result, fields) {
         if(err) {
+            //error so rollback
             con.rollback(function() {
                 throw err;
           });
         } 
         else if(tasks.length === 0) {
+            //if we have no tasks, no need to insert we can just return
             con.commit(function(err) {
                 if (err) { 
                   con.rollback(function() {
@@ -85,6 +105,7 @@ function updateNoteTasks(res, noteId, name, tasks) {
                   });
                 }
                 console.log('Transaction Complete. No tasks.');
+                //return note object
                 res.send({
                     note_id: noteId,
                     name: name
@@ -92,17 +113,22 @@ function updateNoteTasks(res, noteId, name, tasks) {
               });
         }
         else {
+            //need to build our value object
             let values = [];
             for(let i = 0; i < tasks.length; i++) {
                 let row = [];
+                //make an array in the order of the columns we are inserting in
                 row.push(noteId);
                 row.push(tasks[i].idx);
                 row.push(tasks[i].name);
                 row.push(tasks[i].completed);
                 values.push(row);
             }
-            con.query("INSERT INTO tasks (note_id, idx, name, completed) VALUES ?", [values], function(err, result, fields) {
+            //insert all the tasks
+            con.query("INSERT INTO tasks (note_id, idx, name, completed) VALUES ?", [values], 
+                      function(err, result, fields) {
                 if(err) {
+                    //error so rollback
                     con.rollback(function() {
                         throw err;
                   });
@@ -114,6 +140,7 @@ function updateNoteTasks(res, noteId, name, tasks) {
                           });
                         }
                         console.log('Transaction Complete.');
+                        //return note object
                         res.send({
                             note_id: noteId,
                             name: name
@@ -126,38 +153,58 @@ function updateNoteTasks(res, noteId, name, tasks) {
     
 }
 
+/*
+* save a new note, save the tasks, return the note object after save
+*/
 function saveNewNote(res, name, tasks) {
-    con.query("INSERT INTO notes (name) VALUES (?)", [name], function(err, result, fields) {
+    con.query("INSERT INTO notes (name) VALUES (?)", [name], 
+              function(err, result, fields) {
         if(err) {
+            //error so rollback
             con.rollback(function() {
                 throw err;
           });
         } else {
+            //get the noteId of the newly saved object
             let noteId = result.insertId;
+            //save the tasks
             updateNoteTasks(res, noteId, name, tasks);
         }
     });
 }
-              
+
+/*
+* updates an existing note to the provided name, saves the tasks, 
+* and returns the note object after save
+*/
 function updateNote(res, noteId, name, tasks) {
-    con.query("UPDATE notes set name = ? where note_id = ?", [name, noteId], function(err, result, fields) {
+    con.query("UPDATE notes set name = ? where note_id = ?", [name, noteId], 
+              function(err, result, fields) {
         if(err) {
+            //error so rollback
             con.rollback(function() {
                 throw err;
           });
         } else {
+            //now save the tasks
             updateNoteTasks(res, noteId, name, tasks);
         }
     });   
 }
 
+/*
+* post endpoint for saving a note
+*/
 app.post('/save', function(req, res) {
+    //we care about the noteId, its name, and the post body
     let noteId = req.query.noteId;
     let name = req.query.name;
     let body = req.body;
 
+    //open a transaction
     con.beginTransaction(function(err) {
         if (err) { throw err; }
+        //negative note Id means we are a new note
         if(noteId < 0) {
             saveNewNote(res, name, body);
         } else {
@@ -166,27 +213,37 @@ app.post('/save', function(req, res) {
     });
 });
 
-
+/*
+* delete the note with the given id and respond that it was deleted
+*/
 function deleteNote(res, noteId) {
+    //first we delete from tasks since it has a foreign dep on notes
     con.query("DELETE FROM tasks where note_id = ?", [noteId], function(err, result, fields) {
         if(err) {
+            //error so rollback
             con.rollback(function() {
                 throw err;
           });
         } else {
-            con.query("DELETE FROM notes where note_id = ?", [noteId], function(err, result, fields) {
+            //finally we can delete from notes table
+            con.query("DELETE FROM notes where note_id = ?", [noteId], 
+                      function(err, result, fields) {
                 if(err) {
+                    //error so rollback
                     con.rollback(function() {
                         throw err;
                   });
                 } else {
+                    //both deletes succeeded so commit
                     con.commit(function(err) {
                         if (err) { 
+                            //error so rollback
                           con.rollback(function() {
                             throw err;
                           });
                         }
                         console.log('Delete Transaction Complete.');
+                        //respond with the noteId we deleted
                         res.send({
                             note_id: noteId
                         });
@@ -197,29 +254,44 @@ function deleteNote(res, noteId) {
     });
 }
 
+/*
+* delete endpoint for deleting a note
+*/
 app.delete('/', function(req, res) {
+    //open a transaction
     con.beginTransaction(function(err) {
         if (err) { throw err; }
         deleteNote(res, req.query.noteId);
     });
 });
 
+/*
+* serve static tasks css
+*/
 app.get('/tasks.css', function(req, res) {
     res.sendFile(__dirname + "/" + "tasks.css");
 });
 
+/*
+* serve static tasks js
+*/
 app.get('/tasks.js', function(req, res) {
     res.sendFile(__dirname + "/" + "tasks.js");
 });
 
+/*
+* serve static cork board jpg
+*/
 app.get('/cork-board2.jpg', function(req, res) {
     res.sendFile(__dirname + "/" + "cork-board2.jpg");
 });
 
+/*
+* serve static sticky note png
+*/
 app.get('/sticky-note.png', function(req, res) {
     res.sendFile(__dirname + "/" + "sticky-note.png");
 });
 
-//app.listen(process.env.PORT);
 app.listen(process.env.PORT);
 
--- tasks.js
+++ tasks.js
@@ -1,13 +1,26 @@
+
+/*
+* author: Erica Cohen
+* date: 04/21/19
+* class: CSC337 Spring 2019
+* tasks.js - for final project, front end code
+*/
 (function() {
-    
+    "use strict";
+    /**
+    * popup the window with a new note (-1 id, no name, no tasks)
+    */
     function addNote() {
         showPopup(-1, "", []);
     }
     
+    /**
+    * load the tasks for the note and then show the popup
+    */
     function showNote() {
         let noteId = this.getAttribute('note-id');
-        let name = this.querySelector("h4").innerText;
-        fetch("http://localhost:3000/tasks?noteId=" + noteId, {
+        let name = this.querySelector("span").innerText;
+        fetch("/tasks?noteId=" + noteId, {
           method: 'GET'
         }).then(res => res.json())
         .then(resData => {
@@ -15,8 +28,11 @@
         });
     }
     
+    /**
+    * load all the notes and put on the page
+    */
     function loadNotes() {
-        fetch("http://localhost:3000/notes", {
+        fetch("/notes", {
           method: 'GET'
         }).then(res => res.json())
         .then(resData => {
@@ -26,29 +42,43 @@
         });
     }
     
+    /**
+    * add a note the the board based on the data provided (note_id, name)
+    * @param {object} noteData - the noteData of the note to add to the board
+    */
     function addNoteToBoard(noteData) {
         let notes = document.getElementById("notes");
         let snote = document.createElement("span");
         snote.classList = "sticky-note";
         snote.setAttribute('note-id', noteData['note_id']);
-        snote.innerHTML = "<h4>" + noteData.name + "</h4>";
+        snote.innerHTML = "<span>" + noteData.name + "</span>";
 
         snote.onclick = showNote;
         notes.appendChild(snote);
     }
     
+    /**
+    * insert a new task and all its event handlers based on the name/completed
+    * @param {string} name - the name of the task
+    * @param {number} completed - 0/1 is completed
+    */
     function insertTask(name, completed) {
+        let compTasks = document.getElementById("completed");
+        let todoTasks = document.getElementById("todo");
+        
         let task = document.createElement("li");
         task.innerHTML = "<span>" + name + "</span>";
+        //trash can icon
         let del = document.createElement("span");
-        del.innerHTML = "del";
+        del.innerHTML = "<i class=\"fa fa-trash\"></i>";
         del.onclick = function() {
             //grandparent (ul) should remove parent (li)
             this.parentNode.parentNode.removeChild(this.parentNode);
-        }
+        };
         let check = document.createElement("input");
         check.setAttribute("type","checkbox");
         check.onchange = function(){
+            //when checked/unchecked, we need to switch from todo/completed
             let btask= this.parentNode;
             btask.parentNode.removeChild(btask);
             if(this.checked === true){
@@ -57,19 +87,25 @@
             else{
                 todoTasks.appendChild(btask);
             }
-        }
+        };
         task.prepend(check);
         task.appendChild(del);
+        //add to proper list based on original completed value
         if(completed === 1) {
-            document.getElementById("completed").appendChild(task);
+            compTasks.appendChild(task);
             check.checked=true;
         } else {
-            document.getElementById("todo").appendChild(task);
+           todoTasks.appendChild(task);
         }
     }
     
+    /**
+    * add the proper html to the popup given the note information
+    * @param {number} noteId - the noteID
+    * @param {string} name - the name of the note
+    * @param {array} tasks - the array of tasks
+    */
     function showPopup(noteId, name, tasks) {
-        console.log("SHOW", noteId);
         let content = document.getElementById("popupcontent");
         content.setAttribute('note-id', noteId);
         document.getElementById("name").value = name;
@@ -78,10 +114,13 @@
         let todoTasks = document.getElementById("todo");
         let compTasks = document.getElementById("completed");
         
+        //clear out any existing values
+        document.getElementById("newtask").value = "";
         todoTasks.innerHTML = "";
         compTasks.innerHTML = "";
         
         for(let i = 0; i < tasks.length; i++) {
+            //add all the tasks to the popup
             insertTask(tasks[i].name, tasks[i].completed);
         }
         
@@ -90,6 +129,9 @@
         popup.style["display"] = "block";
     }
     
+    /**
+    * add a new task to the popup based on the task name
+    */
     function addNewTask() {
         let task = document.getElementById("newtask");
         let taskName = task.value;
@@ -97,11 +139,17 @@
         insertTask(taskName, 0);
     }
     
+    /**
+    * hide the popup (closed/saved/deleted)
+    */
     function hidePopup() {
         let popup = document.getElementById("popup");
         popup.style["display"] = "none";
     }
     
+    /**
+    * make a request to the backend to save the note
+    */
     function saveNote() {
         let noteId = parseInt(document.getElementById("popupcontent").getAttribute("note-id"));
         
@@ -110,6 +158,7 @@
         let todoTasks = document.getElementById("todo");
         let compTasks = document.getElementById("completed");
         
+        //want to add our tasks in the order todo -> completed
         for(let i = 0; i < todoTasks.childNodes.length; i++) {
             body.push({
                 idx: idx++,
@@ -127,41 +176,45 @@
         }
         
         let name = document.getElementById("name").value;
-        fetch("http://localhost:3000/save?noteId=" + noteId + "&name=" + name, {
+        fetch("/save?noteId=" + noteId + "&name=" + name, {
           method: 'POST',
           body: JSON.stringify(body),
           headers: {
-            "Content-Type": "application/json",
-            // "Content-Type": "application/x-www-form-urlencoded",
+            "Content-Type": "application/json"
           }
         }).then(res => res.json())
         .then(resData => {
-            console.log(resData);
             if(noteId < 0) {
                 //new note
                 addNoteToBoard(resData);
             }
-            hidePopup()
+            hidePopup();
         });
-        console.log("SAVE", noteId, body);
     }
     
+    /**
+    * call to the backend to delete the note
+    */
     function deleteNote() {
         let noteId = parseInt(document.getElementById("popupcontent").getAttribute("note-id"));
-        fetch("http://localhost:3000/?noteId=" + noteId, {
+        fetch("/?noteId=" + noteId, {
           method: 'DELETE'
         }).then(res => res.json())
         .then(resData => {
-            console.log(resData);
             if(noteId === parseInt(resData['note_id'])) {
+                //remove the note from the board
                 let toDel = document.querySelector(".sticky-note[note-id='" + noteId + "']");
                 toDel.parentNode.removeChild(toDel);
             }
-            hidePopup()
+            hidePopup();
         });
     }
     
+    /**
+    * setup event listeners
+    */
     window.onload = function() {
+        hidePopup();
         document.getElementById("add").onclick=addNote;
         
         document.getElementById("close").onclick=hidePopup;
@@ -170,6 +223,5 @@
         document.getElementById("delete").onclick=deleteNote;
         document.getElementById("addtask").onclick=addNewTask;
         loadNotes();
-        hidePopup();
-    }
+    };
 })();
